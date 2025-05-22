7bd51828d13d333aefa12c52d0779b5273db0a04
korolkov Denis <korolkovDK20@yandex.ru>
2025-05-22 20:47:17 +0300

    some changes

--- contorllers/UserInfoController.js
+++ contorllers/UserInfoController.js
@@ -1,35 +1,50 @@
 const User = require("../models/user")
-/** @type {import("express").RequestHandler} */
+
 module.exports = class UserInfoController {
     constructor() {}
-
+/**
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ */
     static async createUser(req, res)
-        {   
+    {   
         try {
             let newUser =  await User.create({
                 'login': req.body.login,
                 'password': req.body.password
             })
-        } catch (error) {
-            return res.json(error.errors[0].message )
+        } catch (error) {           
+            return res.status(500).json(error.errors)
         }
-        return res.json({
+        return res.status(200).json({
             "message" : "user created successfuly"
         })
     }
-
+/**
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ */
     static async getUserInfo (req, res)
-    {
-        let id = req.params.id
-
-        let user = await User.findByPk(id,);
-        return res.json(user);
+    {        
+        if (req.params.id) {
+            let id = req.params.id
+            let user = await User.findByPk(id,);
+            return res.status(200).json(user);
+        } else {
+            return res.status(204).errored()
+        }
     }
-
+/**
+ * @param {import('express').Request} req 
+ * @param {import('express').Response} res 
+ */
     static async updateUserInfo(req, res)
     {
+        console.log(res);
+        
         if (!req.body.id) {
-            return res
+
+            return res.json
         }
         try {
             
